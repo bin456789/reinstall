953d77d9461ddefb321de3ae744763579e0be086
bin456789 <bin456789@gmail.com>
2025-07-15 21:10:41 +0800

    windows: 用户登录后才将 frpc 改成用 LocalService 运行

--- windows-frpc.bat
+++ windows-frpc.bat
@@ -12,7 +12,7 @@ schtasks /Create /TN "frpc" /XML "%SystemDrive%\frpc\frpc.xml"
 schtasks /Run /TN "frpc"
 del "%SystemDrive%\frpc\frpc.xml"
 
-rem win10+ 在首次登录后计划任务才生效
+rem win10+ 在用户首次登录后，用 LocalService 用户运行的计划任务才会生效
 rem 即使手动重启，计划任务也没有运行
 
 rem 如果 10 秒内有 frpc 进程，则代表计划任务已经生效，不需要首次登录
@@ -24,10 +24,15 @@ for /L %%i in (1,1,10) do (
     )
 )
 
-rem 临时改用 SYSTEM 用户运行计划任务，运行后再改回 Local Service 用户
-schtasks /Change /TN "frpc" /RU "S-1-5-18"
-schtasks /Run /TN "frpc"
-schtasks /Change /TN "frpc" /RU "S-1-5-19"
+rem 临时改用 SYSTEM 用户运行计划任务
+schtasks /Change /TN frpc /RU S-1-5-18
+schtasks /Run /TN frpc
+
+rem 用户登录后改回用 LocalService 运行
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f ^
+    /v FrpcRunAsLocalService ^
+    /t REG_SZ ^
+    /d "schtasks /Change /TN frpc /RU S-1-5-19"
 
 :end
 rem 删除此脚本
