df2bda09dce02a182e6717451773528d8e1708a4
bin456789 <bin456789@gmail.com>
2025-09-03 19:36:09 +0800

    windows: 修复链接变动导致无法自动查找 windows server 镜像

--- reinstall.sh
+++ reinstall.sh
@@ -992,7 +992,12 @@ get_windows_iso_link() {
     label_vlsc=$(get_label_vlsc)
     page=$(get_page)
 
-    page_url=https://massgrave.dev/windows_${page}_links
+    if [ "$page" = server ]; then
+        delimiter=-
+    else
+        delimiter=_
+    fi
+    page_url=https://massgrave.dev/windows${delimiter}${page}${delimiter}links
 
     info "Find windows iso"
     echo "Version:    $version"
